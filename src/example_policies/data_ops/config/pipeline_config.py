60329e6ce92621988869a31a828c9440c71f621a
JC <jccalvojackson@gmail.com>
2025-09-16 18:24:26 +0200

    chore: smaller image

--- src/example_policies/data_ops/config/pipeline_config.py
+++ src/example_policies/data_ops/config/pipeline_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import asdict, dataclass, field
+from dataclasses import asdict, dataclass
 
 # String Enum for action levels
 from enum import Enum
@@ -92,9 +92,7 @@ class PipelineConfig:
 
     include_rgb_images: bool = True
     include_depth_images: bool = False
-
-    image_resolution: tuple = field(default_factory=lambda: (256, 256))
-
+    image_resolution: tuple[int, int] = (640, 640)
     depth_scale_factor: float = 1000.0
 
     action_level: ActionLevel = ActionLevel.DELTA_TCP
