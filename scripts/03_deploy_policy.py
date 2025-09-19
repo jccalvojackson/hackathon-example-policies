8592d8f720bebd414b301374c3ad5fa5c0461870
JC <jccalvojackson@gmail.com>
2025-09-19 13:33:04 +0200

    chore; path step 1

--- scripts/03_deploy_policy.py
+++ scripts/03_deploy_policy.py
@@ -5,7 +5,7 @@ from example_policies.robot_deploy import policy_loader
 from example_policies.robot_deploy.deploy import deploy_policy
 
 # CHECKPOINT_DIR = pathlib.Path("outputs/mh2_ckp_step_1_and_2/100000/pretrained_model")
-CHECKPOINT_DIR = pathlib.Path("outputs/vastai/080000/pretrained_model")
+CHECKPOINT_DIR = pathlib.Path("outputs/vastai/125000/pretrained_model")
 CHECKPOINT_DIR_2 = pathlib.Path("outputs/mh2_ckp_step_2/115000/pretrained_model")
 wandb_checkpoint_path = None
 # data/output/checkpoints/last/pretrained_model
