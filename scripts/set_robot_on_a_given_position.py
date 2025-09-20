aa9f5a8dbf8fcf03e70880dc6ac411e5996a91c2
JC <jccalvojackson@gmail.com>
2025-09-20 14:52:31 +0200

    step 1 with reward ds

--- scripts/set_robot_on_a_given_position.py
+++ scripts/set_robot_on_a_given_position.py
@@ -8,7 +8,6 @@ from example_policies.robot_deploy.action_translator import ActionMode, ActionTr
 from example_policies.robot_deploy.robot_io.robot_client import RobotClient
 from example_policies.robot_deploy.robot_io.robot_interface import (
     RobotInterface,
-    _build_cart_target,
 )
 from example_policies.robot_deploy.robot_io.robot_service import (
     robot_service_pb2,
@@ -24,9 +23,18 @@ class DummyConfig:
 def main(server: str, action: torch.Tensor):
     channel = grpc.insecure_channel(server)
     stub = robot_service_pb2_grpc.RobotServiceStub(channel)
+    # prepare_request = robot_service_pb2.PrepareExecutionRequest()
+    # prepare_request.execution_mode = (
+    #     robot_service_pb2.ExecutionMode.EXECUTION_MODE_CARTESIAN_TARGET_QUEUE
+    # )
+    # stub.PrepareExecution(prepare_request)
     cfg = DummyConfig()
     robot_interface = RobotInterface(stub, cfg)
-    robot_interface.send_action(action, ActionMode.ABS_TCP)
+    robot_interface.send_action(
+        action,
+        ActionMode.ABS_TCP,
+        RobotClient.CART_QUEUE,
+    )
 
 
 if __name__ == "__main__":
@@ -92,4 +100,4 @@ if __name__ == "__main__":
             0,
         ]
     )[None, :]
-    main(SERVER_ENDPOINT, tcp_torch_step_3)
+    main(SERVER_ENDPOINT, tcp_torch_step_1)
