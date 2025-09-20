1f1e2e452e03e9b48e9b900a861833cf474f14a9
JC <jccalvojackson@gmail.com>
2025-09-20 11:26:29 +0200

    train step 3 tcp

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,8 +5,8 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_3_tcp_no_joint"
+repo_id = "jccj/mh2_step_3_tcp"
 dataset = LeRobotDataset(
     repo_id=repo_id,
-    root="data/lerobot/step_3_tcp_no_joint",
+    root="data/lerobot/step_3_tcp",
 ).push_to_hub()
